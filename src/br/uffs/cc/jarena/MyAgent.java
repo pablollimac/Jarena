8c8103786400c118537e4a7a27b39d365d964ff2
Pablo <pablo.padilha1999@gmail.com>
2021-08-30 21:45:50 -0300

    Commit teste

---
+++ src/br/uffs/cc/jarena/MyAgent.java
@@ -0,0 +1,49 @@
+package br.uffs.cc.jarena;
+
+public class MyAgent extends Agente {
+	
+	public MyAgent(Integer x, Integer y, Integer energia) {
+		super(x, y, energia);
+		setDirecao(geraDirecaoAleatoria());
+	}
+	
+	public void pensa() {
+		// Se não conseguimos nos mover para a direção atual, quer dizer
+		// que chegamos no final do mapa ou existe algo bloqueando nosso
+		// caminho.
+		if(!podeMoverPara(getDirecao())) {
+			// Como não conseguimos nos mover, vamos escolher uma direção
+			// nova.
+			setDirecao(geraDirecaoAleatoria());
+		}
+		
+		// Se o agente conseguie se dividir (tem energia) e se o total de energia
+		// do agente é maior que 400, nos dividimos. O agente filho terá a metade
+		// da nossa energia atual.
+		if(podeDividir() && getEnergia() >= 800) {
+			divide();
+		}
+	}
+	
+	public void recebeuEnergia() {
+		// Invocado sempre que o agente recebe energia.
+	}
+	
+	public void tomouDano(int energiaRestanteInimigo) {
+		// Invocado quando o agente está na mesma posição que um agente inimigo
+		// e eles estão batalhando (ambos tomam dano).
+	}
+	
+	public void ganhouCombate() {
+		// Invocado se estamos batalhando e nosso inimigo morreu.
+	}
+	
+	public void recebeuMensagem(String msg) {
+		// Invocado sempre que um agente aliado próximo envia uma mensagem.
+	}
+	
+	public String getEquipe() {
+		// Definimos que o nome da equipe do agente é "Fernando".
+		return "Fernando";
+	}
+}
